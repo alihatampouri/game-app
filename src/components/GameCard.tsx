79a7a164eb11b2ba9fda32781590bac3d07de5fa
Ali Hatampouri <ali.hatampouri@gmail.com>
2024-10-20 10:32:47 +0330

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
     return (
-        <Card borderRadius="20px" overflow="hidden">
+        <Card width="300px" borderRadius="20px" overflow="hidden">
             <Image src={getCroppedImageUrl(game.background_image, 600, 400)} />
             <CardBody>
                 <Heading fontSize="2xl">{game.name}</Heading>
