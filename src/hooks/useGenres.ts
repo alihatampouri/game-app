c5a980b145ab13f8e41e9b8120b1aecbe148c0c0
Ali Hatampouri <ali.hatampouri@gmail.com>
2024-10-23 14:33:36 +0330

    Create initial data for genes and platforms

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { Genre } from "../types/genre";
 import apiClient, { FetchResponse } from "../services/api-client";
+import genres from "../data/genres";
+import { Genre } from "../types/genre";
 
 const useGenres = () =>
     useQuery<FetchResponse<Genre>, Error>({
         queryKey: ["genres"],
         queryFn: () => apiClient.get("/genres").then((res) => res.data),
         staleTime: 24 * 60 * 60 * 1000, // 24h
+        initialData: { count: genres.length, results: genres },
     });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,14 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { Platform } from "../types/platform";
 import apiClient, { FetchResponse } from "../services/api-client";
+import platforms from "../data/platforms";
+import { Platform } from "../types/platform";
 
-// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 const usePlatforms = () =>
     useQuery<FetchResponse<Platform>, Error>({
         queryKey: ["paltforms"],
         queryFn: () =>
             apiClient.get("/platforms/lists/parents").then((res) => res.data),
         staleTime: 24 * 60 * 60 * 1000, //24h
+        initialData: { count: platforms.length, results: platforms },
     });
 
 export default usePlatforms;
