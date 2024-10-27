0c13ba2b5eba4cf002d2aec447c189bf70de517a
Ali Hatampouri <ali.hatampouri@gmail.com>
2024-10-27 16:16:15 +0330

    Build game attributes

--- src/types/game.ts
+++ src/types/game.ts
@@ -1,9 +1,13 @@
+import { Genre } from "./genre";
 import { Platform } from "./platform";
+import { Publisher } from "./publisher";
 
 export interface Game {
     id: number;
     name: string;
     slug: string;
+    genres: Genre[];
+    publishers: Publisher[];
     description_raw: string;
     background_image: string;
     parent_platforms: { platform: Platform }[];
